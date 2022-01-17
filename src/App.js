c26a2565f2f9f31f7e17e44516d34f40d7fc0972
ls30378 <ls30378@ubt-uni.net>
2022-01-17 21:06:54 +0100

    new files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
   return (
     <div>
-      <h2>Hello World!</h2>
+      <HomePage />
     </div>
   );
 }
