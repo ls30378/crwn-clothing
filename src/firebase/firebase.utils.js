30f1da93e293a486cb6ae3b6d59ca76bbad3c649
ls30378 <ls30378@ubt-uni.net>
2022-01-21 23:12:15 +0100

    added firebase.utils.js

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import firebase, { initializeApp } from "firebase/compat/app";
+import { getAnalytics } from "firebase/compat/analytics";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBIoOv-P3Bb-CCUnaVPoQsdMl7BJ7X5730",
+    authDomain: "crwn-db-a349b.firebaseapp.com",
+    projectId: "crwn-db-a349b",
+    storageBucket: "crwn-db-a349b.appspot.com",
+    messagingSenderId: "250371500613",
+    appId: "1:250371500613:web:973008f39711bfc57a112c",
+    measurementId: "G-5F0ECGZCH9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
