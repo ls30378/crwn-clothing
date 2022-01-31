2320e0fe9a11a01a047c8bc29dfc0e29cf3a2449
ls30378 <ls30378@ubt-uni.net>
2022-01-31 22:08:46 +0100

    updateed

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,7 +9,7 @@ const middlewares = [logger];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
-}
+};
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
